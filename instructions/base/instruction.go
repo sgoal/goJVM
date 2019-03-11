82c55d853786f2dbadb534b7c5e5fbf127701d6f
sgoal <liuhdsgoal@gmail.com>
2019-03-11 18:14:33 +0800

    add some instruction

---
+++ instructions/base/instruction.go
@@ -0,0 +1,40 @@
+package base
+import "jvmgo/rtda"
+
+type Instruction interface{
+	FetchOpreands (reader *BytecodeReader)
+	Exectue(frame *rtda.Frame)
+}
+
+type NoOperandsInstruction struct{}
+
+func (self *NoOperandsInstruction) FetchOperands(reader *BytecodeReader)  {
+	//nothing to do
+}
+
+//jump
+type BranchInstruction struct{
+	Offset 	int
+}
+
+func (self *BranchInstruction) FetchOperands(reader *BytecodeReader)  {
+	self.Offset = int(reader.ReadInt16())
+}
+
+//local variable index
+type Index8Instruction struct{
+	Index 	uint
+}
+
+func (self *Index8Instruction) FetchOperands(reader *BytecodeReader)  {
+	self.Index = uint(reader.ReadInt8())
+}
+
+//local constant pool index
+type Index16Instruction struct{
+	Index 	uint
+}
+
+func (self *Index16Instruction) FetchOperands(reader *BytecodeReader)  {
+	self.Index = uint(reader.ReadInt16())
+}
---
+++ instructions/constants/nop.go
@@ -0,0 +1,9 @@
+package constants
+import "jvmgo/instructions/base"
+import "jvmgo/rtda"
+
+type NOP struct{ base.NoOperandsInstruction}
+
+func (self *NOP) Execute(frame *rtda.Frame)  {
+	//nothing to do
+}
